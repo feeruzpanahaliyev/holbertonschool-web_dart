8131944513ee09fef0c3124b5a4bd58382f2b974
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-10-17 20:59:04 +0400

    Update 6-inheritance.dart

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -1,48 +1,45 @@
 import '6-password.dart';
 
 class User extends Password {
-  String? name;
-  int? age;
-  double? height;
-  int? id;
-  String user_password = '';
+  int id;
+  String name;
+  int age;
+  double height;
+  late String user_password;
 
-  User(
-      {id = int,
-      name = String,
-      age = int,
-      height = double,
-      user_password = String})
-      : super(password: user_password) {
-    this.id = id;
-    this.name = name;
-    this.age = age;
-    this.height = height;
+  User({
+    required this.id,
+    required this.name,
+    required this.age,
+    required this.height,
+    required String user_password,
+  }) : super(password: user_password) {
     this.user_password = user_password;
   }
 
   static User fromJson(Map<dynamic, dynamic> userJson) {
     return User(
-        id: userJson['id'],
-        name: userJson['name'],
-        age: userJson['age'],
-        height: userJson['height'],
-        user_password: '');
+      id: userJson['id'] ?? 0,
+      name: userJson['name'] ?? '',
+      age: userJson['age'] ?? 0,
+      height: userJson['height'] ?? 0.0,
+      user_password: userJson['user_password'] ?? '',
+    );
   }
 
-  Map toJson() {
+  Map<String, dynamic> toJson() {
     return {
-      'id': this.id,
-      'name': this.name,
-      'age': this.age,
-      'height': this.height
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height,
+      'user_password': user_password,
     };
   }
 
   @override
   String toString() {
-    final pass = new Password(password: this.user_password);
-
-    return 'User(id : ${this.id} ,name: ${this.name}, age: ${this.age}, height: ${this.height}, Password: ${pass.isValid()})';
+    bool passwordValid = super.isValid();
+    return 'User(id : $id ,name: $name, age: $age, height: $height, Password: $passwordValid)';
   }
 }
