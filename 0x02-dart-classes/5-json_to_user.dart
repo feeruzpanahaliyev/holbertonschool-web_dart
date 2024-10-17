a7254b3f415a94105f577c1e95f728d6e08a3209
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-10-17 20:31:52 +0400

    Update 5-json_to_user.dart

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -1,31 +1,40 @@
 class User {
+  int id;          // New property for User ID
   String name;
   int age;
   double height;
-  int id;
 
-  User({required this.name, required this.age, required this.height, required this.id});
+  // Constructor to initialize all properties
+  User({
+    required this.id,   // Include id in constructor
+    required this.name,
+    required this.age,
+    required this.height,
+  });
 
+  // Method to convert User object to JSON
   Map<String, dynamic> toJson() {
     return {
+      'id': id,         // Include id in JSON output
       'name': name,
       'age': age,
       'height': height,
-      'id': id,
     };
   }
 
+  // Static method to create User instance from JSON Map
   static User fromJson(Map<dynamic, dynamic> userJson) {
     return User(
-      id: userJson['id'],
-      name: userJson['name'],
-      age: userJson['age'],
-      height: userJson['height'],
+      id: userJson['id'],                // Retrieve id from Map
+      name: userJson['name'],            // Retrieve name from Map
+      age: userJson['age'],              // Retrieve age from Map
+      height: userJson['height'],        // Retrieve height from Map
     );
   }
 
+  // Method to return a string representation of the User object
   @override
   String toString() {
-    return 'User(id : $id ,name: $name,  age: $age, height: $height)';
+    return 'User(id: $id, name: $name, age: $age, height: $height)';
   }
 }
