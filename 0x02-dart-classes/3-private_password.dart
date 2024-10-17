48716ea11b5dab5c8344b90c8d8ab6c74f9b2bb2
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-10-17 20:01:26 +0400

    Update 3-private_password.dart

--- 0x02-dart-classes/3-private_password.dart
+++ 0x02-dart-classes/3-private_password.dart
@@ -2,17 +2,21 @@ class Password {
   String _password;
 
   Password({required String password}) : _password = password;
-  bool isValid() {
-    final hasUppercase = _password.contains(RegExp(r'[A-Z]'));
-    final hasLowercase = _password.contains(RegExp(r'[a-z]'));
-    final hasDigit = _password.contains(RegExp(r'[0-9]'));
-    final lengthValid = _password.length >= 8 && _password.length <= 16;
 
-    return hasUppercase && hasLowercase && hasDigit && lengthValid;
+  bool isValid() {
+    // Check if the password length is between 8 and 16
+    if (_password.length < 8 || _password.length > 16) return false;
+
+    // Check for uppercase, lowercase, and numeric characters
+    bool hasUppercase = _password.contains(RegExp(r'[A-Z]'));
+    bool hasLowercase = _password.contains(RegExp(r'[a-z]'));
+    bool hasNumber = _password.contains(RegExp(r'[0-9]'));
+
+    return hasUppercase && hasLowercase && hasNumber;
   }
 
   @override
   String toString() {
-    return 'Your Password is $_password';
+    return "Your Password is $_password";
   }
 }
