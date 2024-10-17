2b4dec94d7de529bc44c9bd44c74b3448f31dcfb
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-10-17 20:26:16 +0400

    Update 3-private_password.dart

--- 0x02-dart-classes/3-private_password.dart
+++ 0x02-dart-classes/3-private_password.dart
@@ -1,29 +1,17 @@
 class Password {
-  // Private password property
+
   String _password;
-
-  // Constructor that initializes the private password property
   Password({required String password}) : _password = password;
-
   String get password => _password;
 
-  // Method to validate the password according to given conditions
   bool isValid() {
-    // Check if the password length is between 8 and 16 characters
-    if (_password.length < 8 || _password.length > 16) return false;
-
-    // Check for uppercase, lowercase, and numeric characters
-    bool hasUppercase = _password.contains(RegExp(r'[A-Z]'));
-    bool hasLowercase = _password.contains(RegExp(r'[a-z]'));
-    bool hasNumber = _password.contains(RegExp(r'[0-9]'));
-
-    // Return true only if all conditions are met
-    return hasUppercase && hasLowercase && hasNumber;
+     return ((password.length >= 8) &&
+     (password.length <= 16)) &&
+        this.password.contains(new RegExp(r"[A-Z]"))
+	&& this.password.contains(new RegExp(r"[a-z]"))
+	&& this.password.contains(new RegExp(r"[0-9]"));
   }
 
-  // Override the toString method to provide a custom output for the password
   @override
-  String toString() {
-    return "Your Password is $_password";
-  }
+  String toString() => "Your Password is: ${this.password}";
 }
