415a78cc1185071c1ec19217a73192704d75d81f
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-10-17 20:55:17 +0400

    Update 6-inheritance.dart

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -6,20 +6,24 @@ class User extends Password {
   int age;
   double height;
 
+  // New property to hold the user password
+  String user_password;
+
+  // Constructor to initialize the User
   User({
     required this.id,
     required this.name,
     required this.age,
     required this.height,
-    required String user_password,
-  }) : super(password: user_password); // Pass password to Password superclass
+    required this.user_password,
+  }) : super(password: user_password) {
+    // Update the password in the superclass
+    password = user_password;
+  }
 
-  // Getter for user_password
-  String get user_password => password; // Access password via getter
-
-  // Setter for user_password
+  // Override the setter for user_password to also update the superclass
   set user_password(String newPassword) {
-    password = newPassword; // Update password
+    password = newPassword; // Update the password in the Password superclass
   }
 
   // Convert User to JSON
@@ -37,14 +41,14 @@ class User extends Password {
   static User fromJson(Map<dynamic, dynamic> userJson) {
     return User(
       id: userJson['id'] ?? 0, // Default value if null
-      name: userJson['name'] ?? '',
-      age: userJson['age'] ?? 0,
-      height: userJson['height'] ?? 0.0,
-      user_password: userJson['user_password'] ?? '',
+      name: userJson['name'] ?? '', // Default value if null
+      age: userJson['age'] ?? 0, // Default value if null
+      height: userJson['height']?.toDouble() ?? 0.0, // Default value if null
+      user_password: userJson['user_password'] ?? '', // Default value if null
     );
   }
 
-  // Override toString method
+  // Override toString method to include password validity
   @override
   String toString() {
     return "User(id: $id, name: $name, age: $age, height: $height, Password: ${isValid()})";
