6d1af4c145c16eb9f49675186a33a0375c5e0b08
feeruzpanahaliyev <156419117+feeruzpanahaliyev@users.noreply.github.com>
2024-10-17 20:51:18 +0400

    Update 6-inheritance.dart

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -5,7 +5,6 @@ class User extends Password {
   String name;
   int age;
   double height;
-  late String user_password;
 
   User({
     required this.id,
@@ -13,20 +12,17 @@ class User extends Password {
     required this.age,
     required this.height,
     required String user_password,
-  }) : super(password: user_password) {
-    this.user_password = user_password;
+  }) : super(password: user_password); // Pass password to Password superclass
+
+  // Getter for user_password
+  String get user_password => password; // Access password via getter
+
+  // Setter for user_password
+  set user_password(String newPassword) {
+    password = newPassword; // Update password
   }
 
-  static User fromJson(Map<dynamic, dynamic> userJson) {
-    return User(
-      id: userJson['id'] ?? 0,
-      name: userJson['name'] ?? '',
-      age: userJson['age'] ?? 0,
-      height: userJson['height'] ?? 0.0,
-      user_password: userJson['user_password'] ?? '',
-    );
-  }
-
+  // Convert User to JSON
   Map<String, dynamic> toJson() {
     return {
       'id': id,
@@ -37,9 +33,20 @@ class User extends Password {
     };
   }
 
+  // Static method to create a User instance from JSON
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'] ?? 0, // Default value if null
+      name: userJson['name'] ?? '',
+      age: userJson['age'] ?? 0,
+      height: userJson['height'] ?? 0.0,
+      user_password: userJson['user_password'] ?? '',
+    );
+  }
+
+  // Override toString method
   @override
   String toString() {
-    bool passwordValid = super.isValid();
-    return 'User(id : $id ,name: $name, age: $age, height: $height, Password: $passwordValid)';
+    return "User(id: $id, name: $name, age: $age, height: $height, Password: ${isValid()})";
   }
 }
